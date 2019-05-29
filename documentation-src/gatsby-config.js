05747f98f2b5021245cdb1c9c12d7f6821581715
Klem <kaelhem@gmail.com>
2019-05-29 19:54:45 +0200

    another try

--- documentation-src/gatsby-config.js
+++ documentation-src/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         name: "KatPat",
         short_name: "KatPat",
-        start_url: ".",
+        start_url: "https://kaelhem.github.io/katpat/documentation",
         background_color: "#93d12f",
         theme_color: "#93d12f",
         display: "standalone",
